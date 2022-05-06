71b3ad412cf921fc002469042f288c75189496f5
ritesh2912 <94890713+ritesh2912@users.noreply.github.com>
2022-05-06 08:38:00 +0530

    Delete depend.sh

--- depend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu
-sudo apt update
-sudo apt install python3-pip python3-dev nginx curl
-pip install virtualenv
-virtualenv env
-source env/bin/activate
-pip install django gunicorn
