063f13e8c6d77fc6ef77ab43a2bb317bcc14247b
ritesh2912 <94890713+ritesh2912@users.noreply.github.com>
2022-05-09 09:36:29 +0530

    Update settings.py

--- first/settings.py
+++ first/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ty6e($3bdwn2^^dv%*wk2s3*iwz9ix0sh!k0&ok_o)2w%8jaa*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.110.92.104']
+ALLOWED_HOSTS = ['13.233.101.132']
 
 
 # Application definition
