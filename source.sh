2e2fa64db06d488fac394d769f8a1ca32994a4c0
ritesh2912 <94890713+ritesh2912@users.noreply.github.com>
2022-05-09 06:30:02 +0530

    Update source.sh

--- source.sh
+++ source.sh
@@ -33,7 +33,7 @@ sudo systemctl enable gunicorn.socket
 
 echo "server {
     listen 80;
-    server_name 3.111.41.184;
+    server_name 3.110.92.104;
     location = /favicon.ico { access_log off; log_not_found off; }
     location /static/ {
         root /home/ubuntu/projectdir/projectdir;
