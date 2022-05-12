f76d96846adf6ee4bcdb81747000c2a3d2c028b9
ritesh2912 <94890713+ritesh2912@users.noreply.github.com>
2022-05-12 13:37:19 +0530

    Update second.sh

--- second.sh
+++ second.sh
@@ -4,6 +4,6 @@ cd /home/ubuntu
 sudo apt update
 sudo apt install python3-pip python3-dev nginx -y
 pip install virtualenv
-virtualenv env
-source env/bin/activate
+virtualenv myprojectenv
+source myprojectenv/bin/activate
 pip install django gunicorn
